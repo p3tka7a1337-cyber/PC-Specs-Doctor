96561c8d65652ebca42ec9c45304be8dc74053f4
alex3733379 <aleksandar.paskalev.24@trakia-uni.bg>
2026-04-22 18:27:01 +0300

    Added a check + advice system

--- doctor.py
+++ doctor.py
@@ -1,1 +1,7 @@
-
+def give_advice(user_specs, game_req):
+    advice = []
+    if user_specs['ram'] < game_req['ram']:
+        advice.append(f"Добавете поне {game_req['ram'] - user_specs['ram']}GB RAM.")
+    if not advice:
+        return "Системата е перфектна!"
+    return "Съвет: " + " ".join(advice)
