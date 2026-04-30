81b7f8f38e418d34a17c4e767aa1f5b6160b7652
k1ckd0wn <40430136+k1ckd0wnx@users.noreply.github.com>
2026-04-30 14:19:52 +0300

    Add GPU requirement advice for game specifications

--- doctor.py
+++ doctor.py
@@ -4,6 +4,8 @@ def give_advice(user_specs, game_req):
         advice.append(f"Добавете поне {game_req['ram'] - user_specs['ram']}GB RAM.")
     if user_specs['cpu'] < game_req['cpu']:
         advice.append(f"Процесорът е слаб (трябват поне {game_req['cpu']} ядра).")
+    if user_specs['gpu'] < game_req['gpu']:
+        advice.append(f"Видеокартата е слаба (трябва поне {game_req['gpu']} RAM).")
     if not advice:
         return "Системата е перфектна!"
     return "Съвет: " + " ".join(advice)
