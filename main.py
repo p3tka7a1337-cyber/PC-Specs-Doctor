17c15ee26927ba54ee61015680b561518f196bde
alex3733379 <aleksandar.paskalev.24@trakia-uni.bg>
2026-04-30 14:32:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -116,4 +116,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"\n❌ Възникна грешка: {e}")
+        input("\nНатиснете Enter, за да затворите...")
