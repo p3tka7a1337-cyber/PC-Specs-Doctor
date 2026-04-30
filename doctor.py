eed00da191b72a09f6b0dbd3a39d5f03a708b82c
k1ckd0wn <40430136+k1ckd0wnx@users.noreply.github.com>
2026-04-30 14:04:03 +0300

    Add CPU advice to give_advice function

--- doctor.py
+++ doctor.py
@@ -2,6 +2,8 @@ def give_advice(user_specs, game_req):
     advice = []
     if user_specs['ram'] < game_req['ram']:
         advice.append(f"Добавете поне {game_req['ram'] - user_specs['ram']}GB RAM.")
+    if user_specs['cpu'] < game_req['cpu']:
+        advice.append(f"Процесорът е слаб (трябват поне {game_req['cpu']} ядра).")
     if not advice:
         return "Системата е перфектна!"
     return "Съвет: " + " ".join(advice)
